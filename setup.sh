f71ed34a730e9231369fd7d9c039259959d4427a
Colin Bendell <colin@bendell.ca>
2025-10-01 15:00:42 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,71 +1,55 @@
 #!/usr/bin/env sh
 cd $HOME
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+
+curl_install() {
+  local name="$1"
+  local install_path="$2"
+  local url="$3"
+
+  if [ -e "$install_path" ]; then
+    echo "SKIP: $name"
+  else
+    sh -c "$(curl -fsSL $url)" "" --unattended
+    echo "✅ $name"
+  fi
+}
+
+git_clone() {
+  local name="$1"
+  local repo_url="$2"
+  local install_path="$3"
+
+  if [ -d "$install_path" ]; then
+    echo "SKIP: $name"
+  else
+    git clone --single-branch --depth=1 "$repo_url" "$install_path"
+    echo "✅ $name"
+  fi
+}
 
 # Install oh-my-zsh
-echo "Installing Oh My Zsh..."
-if [ -d "$HOME/.oh-my-zsh" ]; then
-  echo "Oh My Zsh is already installed. Skipping installation."
-else
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  echo "Oh My Zsh installed successfully."
-fi
+curl_install "Oh My Zsh" $HOME/.oh-my-zsh "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
 
-# Install zsh-autosuggestions
-echo "Installing the zsh-autosuggestions plugin..."
-if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-  echo "zsh-autosuggestions is already installed. Skipping installation."
-else
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  echo "zsh-autosuggestions installed successfully."
-fi
+# Install zsh plugins and themes
+git_clone "zsh-autosuggestions" "https://github.com/zsh-users/zsh-autosuggestions" "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+git_clone "zsh-syntax-highlighting" "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+git_clone "zsh-bat" "https://github.com/fdellwing/zsh-bat.git" "$ZSH_CUSTOM/plugins/zsh-bat"
+git_clone "you-should-use" "https://github.com/MichaelAquilina/zsh-you-should-use.git" "$ZSH_CUSTOM/plugins/you-should-use"
+git_clone "powerlevel10k" "https://github.com/romkatv/powerlevel10k.git" "$ZSH_CUSTOM/themes/powerlevel10k"
 
-# Install zsh-syntax-highlighting
-echo "Installing the zsh-syntax-highlighting plugin..."
-if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-  echo "zsh-syntax-highlighting is already installed. Skipping installation."
-else
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-  echo "zsh-syntax-highlighting installed successfully."
-fi
-
-# Install powerlevel10k
-echo "Installing the powerlevel10k plugin..."
-if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/powerlevel10k" ]; then
-  echo "powerlevel10k is already installed. Skipping installation."
-else
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  echo "powerlevel10k installed successfully."
-fi
-
-# Install zsh-bat
-echo "Installing the zsh-bat plugin..."
-if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-bat" ]; then
-  echo "zsh-bat is already installed. Skipping installation."
-else
-  git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/zsh-bat
-  echo "zsh-bat installed successfully."
-fi
-
-# Install you-should-use
-echo "Installing the you-should-use plugin..."
-if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/you-should-use" ]; then
-  echo "you-should-use is already installed. Skipping installation."
-else
-  git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/you-should-use
-  echo "you-should-use installed successfully."
-fi
-
-# Install you-should-use
-echo "Installing the oh-my-tmux..."
-if [ -d "$HOME/.tmux" ]; then
-  echo "oh-my-tmux is already installed. Skipping installation."
-else
-  git clone --single-branch https://github.com/gpakosz/.tmux.git $HOME/.tmux
+git_clone "oh-my-tmux" "https://github.com/gpakosz/.tmux.git" "$HOME/.tmux"
+if [ ! -f "$HOME/.tmux.conf" ]; then
   ln -s -f .tmux/.tmux.conf
-
-  echo "oh-my-tmux installed successfully."
+  echo "✅ linking tmux.conf"
 fi
 
-if [ -f "Brewfile" ]; then
-  brew bundle
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Install packages via Homebrew (macOS only)
+  curl_install "Homebrew" "/opt/homebrew/bin/brew" "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+
+  if [ -f "Brewfile" ]; then
+    echo "✅ Homebrew packages"
+    brew bundle
+  fi
 fi
